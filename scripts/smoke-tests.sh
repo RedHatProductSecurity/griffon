c2ce77aa7cb5f83d4c1d3145c6ed8b16e792db4b
jfuller <jfuller@redhat.com>
2023-04-05 10:28:54 +0200

    fix/amend smoke tests

--- scripts/smoke-tests.sh
+++ scripts/smoke-tests.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
+
 # primitive smoke test
 
 # service queries
-griffon service component-manifest --purl "pkg:oci/ubi8-minimal-container@sha256:7679eaafa608171dd159a91529804d06fa0fbc16a2ea7f046a592a5d8e22c649?repository_url=registry.redhat.io/ubi8-minimal&tag=8.8-315"
+#griffon service component-manifest --purl "pkg:oci/ubi8-minimal-container@sha256:7679eaafa608171dd159a91529804d06fa0fbc16a2ea7f046a592a5d8e22c649?repository_url=registry.redhat.io/ubi8-minimal&tag=8.8-315"
 griffon service components-affected-by-flaw CVE-2023-25166 --type NPM
 griffon service components-contain-component --purl "pkg:rpm/redhat/zlib@1.2.11-21.el8_7?arch=x86_64"
 griffon service product-components --ofuri o:redhat:openshift:4.8.z
@@ -27,29 +29,30 @@ griffon service component-summary curl
 griffon service report-affects
 
 # products
-griffon entities product-streams get pipelines-1.6.2
-griffon entities product-streams list ansible
+griffon entities CORGI product-streams get pipelines-1.6.2
+griffon entities CORGI product-streams list ansible
 
 # components
-griffon entities components list curl
-griffon entities components list curl --namespace UPSTREAM
-griffon entities components get --purl "pkg:rpm/redhat/vim@8.2.2637-16.el9_0.3?arch=src&epoch=2"
-griffon entities components manifest --purl "pkg:rpm/curl@7.76.1"
-griffon entities components list --ofuri o:redhat:ansible_automation_platform:2.2 --type OCI
-griffon --format text entities components list curl
+griffon entities CORGI components list curl
+griffon entities CORGI components list curl --namespace UPSTREAM
+griffon entities CORGI components get --purl "pkg:rpm/redhat/vim@8.2.2637-16.el9_0.3?arch=src&epoch=2"
+griffon entities CORGI components manifest --purl "pkg:rpm/curl@7.76.1"
+griffon entities CORGI components list --ofuri o:redhat:ansible_automation_platform:2.2 --type OCI
+griffon --format text entities CORGI components list curl
 
 # flaws
-griffon entities flaws get --cve-id CVE-2023-25166
-griffon entities flaws list --state NEW --impact CRITICAL
+griffon entities OSIDB flaws get --cve-id CVE-2023-25166
+griffon entities OSIDB flaws list --state NEW --impact CRITICAL
 
 # affects
-griffon entities affects list --affectedness AFFECTED --impact CRITICAL
+griffon entities OSIDB affects list --affectedness AFFECTED --impact CRITICAL
 
 # trackers
-griffon entities trackers list --help
+griffon entities OSIDB trackers list --help
 
 # manage
-griffon manage corgi health
+griffon entities CORGI admin health
+griffon entities OSIDB admin health
 
 # plugins
 griffon plugins go_vuln get --cve-id CVE-2018-16873
