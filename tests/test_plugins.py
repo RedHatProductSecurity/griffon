cf32924cbcaf10bf4ff868b85bf1f68fd2076e50
jfuller <jfuller@redhat.com>
2023-04-06 08:00:47 +0200

    add simple semgrep plugin

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -16,3 +16,27 @@ def test_fcc():
     runner = CliRunner()
     result = runner.invoke(cli, ["plugins", "fcc"])
     assert result.exit_code == 0
+
+
+def test_semgrep():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["plugins", "semgrep"])
+    assert result.exit_code == 0
+
+
+def test_go_vuln():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["plugins", "go_vuln"])
+    assert result.exit_code == 0
+
+
+def test_cve_mitre():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["plugins", "cve_mitre"])
+    assert result.exit_code == 0
+
+
+def test_cvelib():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["plugins", "cvelib"])
+    assert result.exit_code == 0
