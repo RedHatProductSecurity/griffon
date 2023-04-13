9c5dcbf0ea9245b1701de97854e223daf90ec4a1
jfuller <jfuller@redhat.com>
2023-04-13 11:36:55 +0200

    prep release v0.1.18

--- griffon/__init__.py
+++ griffon/__init__.py
@@ -12,7 +12,7 @@ from rich.logging import RichHandler
 
 from griffon.output import console
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 if "CORGI_API_URL" not in os.environ:
     print("Must set CORGI_API_URL environment variable.")
