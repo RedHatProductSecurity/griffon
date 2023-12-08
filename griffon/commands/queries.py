fa14c8453509f0782b802f9ef366acb55b6e90d1
Jakub Frejlach <jfrejlac@redhat.com>
2023-12-08 13:24:17 +0100

    Fix filtering middleware data
    * consider all middleware data retrieved by middleware CLI active
    * delegate maven build type filter to middleware CLI

--- griffon/commands/queries.py
+++ griffon/commands/queries.py
@@ -406,7 +406,15 @@ def get_product_contain_component(
         operation_status.update("searching deptopia middleware.")
 
         # Use split for users who runs middleware via python
-        mw_command = [*MIDDLEWARE_CLI.split(), component_name, "-e", "maven", "--json"]
+        mw_command = [
+            *MIDDLEWARE_CLI.split(),
+            component_name,
+            "-e",
+            "maven",
+            "-b",
+            "maven",
+            "--json",
+        ]
         if strict_name_search:
             mw_command.append("-s")
         proc = subprocess.run(
@@ -421,39 +429,39 @@ def get_product_contain_component(
             # if search_all:
             #     mw_components.extend(mw_json["deps"])
             for build in mw_components:
-                if build["build_type"] == "maven":
-                    component = {
-                        "product_versions": [{"name": build["ps_module"]}],
-                        "product_streams": [
-                            {
-                                "name": build["ps_update_stream"],
-                                "product_versions": [{"name": build["ps_module"]}],
-                            }
-                        ],
-                        "product_active": True,
-                        "type": build["build_type"],
-                        "name": build["build_name"],
-                        "nvr": build["build_nvr"],
-                        "upstreams": [],
-                        "sources": [],
-                        "software_build": {
-                            "build_id": build["build_id"],
-                            "source": build["build_repo"],
-                        },
-                    }
-                    if "sources" in build:
-                        for deps in build["sources"]:
-                            for dep in deps["dependencies"]:
-                                components = []
-                                components.append(
-                                    {
-                                        "name": dep.get("name"),
-                                        "nvr": dep.get("nvr"),
-                                        "type": dep.get("type"),
-                                    }
-                                )
-                                component["sources"] = components
-                    q.append(component)
+                component = {
+                    "product_versions": [{"name": build["ps_module"]}],
+                    "product_streams": [
+                        {
+                            "name": build["ps_update_stream"],
+                            "product_versions": [{"name": build["ps_module"]}],
+                            "active": True,  # assume all product streams as active
+                        }
+                    ],
+                    "product_active": True,
+                    "type": build["build_type"],
+                    "name": build["build_name"],
+                    "nvr": build["build_nvr"],
+                    "upstreams": [],
+                    "sources": [],
+                    "software_build": {
+                        "build_id": build["build_id"],
+                        "source": build["build_repo"],
+                    },
+                }
+                if "sources" in build:
+                    for deps in build["sources"]:
+                        for dep in deps["dependencies"]:
+                            components = []
+                            components.append(
+                                {
+                                    "name": dep.get("name"),
+                                    "nvr": dep.get("nvr"),
+                                    "type": dep.get("type"),
+                                }
+                            )
+                            component["sources"] = components
+                q.append(component)
         except Exception:
             logger.warning("problem accessing deptopia.")
 
